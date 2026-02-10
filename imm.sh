9013355c38ab288c314f8ece014bc96dcdb4a943
lxfx-code <lxfxmy@gmail.com>
2026-02-10 08:19:43 +0800

    Update imm.sh

--- imm.sh
+++ imm.sh
@@ -3,7 +3,7 @@ mkdir -p imm
 #https://github.com/wukongdaily/AutoBuildImmortalWrt/releases/download/Autobuild-x86-64/immortalwrt-24.10.0-x86-64-generic-squashfs-combined-efi.img.gz
 
 REPO="lxfx-code/AutoBuildImmortalWrt"
-TAG="img-installer"
+TAG="Autobuild-x86-64"
 FILE_NAME="immortalwrt-24-7G-x86-efi.img.gz"
 OUTPUT_PATH="imm/immortalwrt.img.gz"
 
